a80a941db2b6099c02128d3049163b84f9208de9
Denis Anisimov <vysheradugi4@gmail.com>
2019-04-08 11:16:51 -0400

    Docs fixed

--- silly-datatable-documentation/src/app/documentation/options/options.component.ts
+++ silly-datatable-documentation/src/app/documentation/options/options.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-options',
   templateUrl: './options.component.html',
-  styleUrls: ['./options.component.scss']
+  styleUrls: ['./options.component.scss'],
 })
 export class OptionsComponent implements OnInit {
 
