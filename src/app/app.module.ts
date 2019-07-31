983989b052d202103388dd83abb74448516ad4c3
Denis Anisimov <vysheradugi4@gmail.com>
2019-07-31 07:59:38 -0400

    Updated dependecies, fixed docs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
-import { SillyDatatableModule } from 'silly-datatable';
 import { EditButtonComponent } from 'src/app/shared/components/edit-button/edit-button.component';
 import { ComponentsModule } from './shared/components/components.module';
 import { SecondComponent } from './second/second.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
-import { RouterModule } from '@angular/router';
 import { FirstComponent } from './first/first.component';
+import { SillyDatatableModule } from 'projects/silly-datatable/src/public_api';
 
 
 @NgModule({
