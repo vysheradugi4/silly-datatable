cb25056f0a8d3927d1a4329dd511101cc3aa1e50
Denis Anisimov <vysheradugi4@gmail.com>
2020-06-04 11:41:30 -0400

    Disable control directive fixed

--- projects/silly-datatable/src/lib/shared/directives/disable-control.directive.ts
+++ projects/silly-datatable/src/lib/shared/directives/disable-control.directive.ts
@@ -1,17 +1,23 @@
 import { NgControl } from '@angular/forms';
-import { Directive, Input, Optional } from '@angular/core';
+import { Directive, Input, Optional, OnChanges, SimpleChange } from '@angular/core';
 
 @Directive({
   // tslint:disable-next-line:directive-selector
   selector: '[disableControl]',
 })
-export class DisableControlDirective {
+export class DisableControlDirective implements OnChanges {
 
-  @Input() set disableControl(condition: boolean) {
-    const action = condition ? 'disable' : 'enable';
-    this._ngControl.control[action]({ emitEvent: false });
-  }
+  @Input() public disableControl: boolean;
 
-  constructor(@Optional() private _ngControl: NgControl) {
+  constructor(
+    @Optional() private _ngControl: NgControl
+  ) { }
+
+
+  ngOnChanges(changes): void {
+    if (changes['disableControl']) {
+      const action = this.disableControl ? 'disable' : 'enable';
+      this._ngControl.control[action]({ emitEvent: false });
+    }
   }
 }
